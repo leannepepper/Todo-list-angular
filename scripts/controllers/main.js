af74e7f4cc156c9d246904e277088428e7d6a33a
LeannePepper <leanne.werner.lw@gmail.com>
2019-04-11 12:17:58 -0700

    added list item styles

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -14,12 +14,12 @@ angular.module('todoListApp')
     $scope.helloConsole = dataService.helloConsole;  
     
     $scope.todos = [
-        {"name": "feed the dog"},
-        {"name": "pet the dog"},
-        {"name": "play with the dog"},
-        {"name": "Tell the dog you love him"},
-        {"name": "Hold the dog untill he barks"},
-        {"name": "Say hi to husband"}
+        {"name": "Feed the Dog"},
+        {"name": "Pet the Dog"},
+        {"name": "Play with the Dog"},
+        {"name": "Tell the Dog you Love him"},
+        {"name": "Hold the Dog Untill he Barks"},
+        {"name": "Say hi to Husband"}
     ];
 
     $scope.deleteTodo = function(todo, $index) {
